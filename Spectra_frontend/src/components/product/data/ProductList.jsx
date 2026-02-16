3c2d13fb48fe99d59c308df9acd9d675ed9d6064
NA <nguyenchaunhatanh1008@gmail.com>
2026-02-16 18:33:40 +0700

    build ProductList and ProductCard UI with id, name, price, image, rating and navigation

---
+++ Spectra_frontend/src/components/product/data/ProductList.jsx
@@ -0,0 +1,37 @@
+import pic1 from "../../../img/3217012-eyeglasses-angle-view.avif";
+import pic4 from "../../../img/2010621-eyeglasses-angle-view.avif";
+import pic7 from "../../../img/125221-eyeglasses-angle-view.avif";
+import pic11 from "../../../img/2020123-eyeglasses-angle-view.avif";
+
+const ProductList = [
+    {
+        id: "GM001",
+        name: "Graphite Point Guard Glasses",
+        price: 15.95,     
+        rating: 4.6,      
+        image: pic1,
+    },
+    {
+        id: "GM002",
+        name: "Black Rectangle Glasses",
+        price: 18.5,
+        rating: 4.8,
+        image: pic4,
+    },
+    {
+        id: "GM003",
+        name: "Black Klutch Player Glasses",
+        price: 14.95,
+        rating: 4.5,
+        image: pic7,
+    },
+    {
+        id: "GM004",
+        name: "Clear Square Glasses",
+        price: 15.95,
+        rating: 4.5,
+        image: pic11,
+    },
+];
+
+export default ProductList;
