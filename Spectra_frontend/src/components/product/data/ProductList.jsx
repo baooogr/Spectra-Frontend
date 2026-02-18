5700ed35ccb4d4e73348d6faa4925e8e5e73a3bc
NA <nguyenchaunhatanh1008@gmail.com>
2026-02-18 17:32:07 +0700

    update ProductList to use ImageGallery with thumbnail selection

--- Spectra_frontend/src/components/product/data/ProductList.jsx
+++ Spectra_frontend/src/components/product/data/ProductList.jsx
@@ -1,36 +1,52 @@
-import pic1 from "../../../img/3217012-eyeglasses-angle-view.avif";
-import pic4 from "../../../img/2010621-eyeglasses-angle-view.avif";
-import pic7 from "../../../img/125221-eyeglasses-angle-view.avif";
-import pic11 from "../../../img/2020123-eyeglasses-angle-view.avif";
+import pic1 from "../img/3217012-eyeglasses-angle-view.avif";
+import pic2 from "../img/3217012-eyeglasses-front-view.avif";
+import pic3 from "../img/3217012-eyeglasses-side-view.avif";
+
+import pic4 from "../img/2010621-eyeglasses-angle-view.avif";
+import pic5 from "../img/2010621-eyeglasses-front-view.avif";
+import pic6 from "../img/2010621-eyeglasses-side-view.avif";
+
+import pic7 from "../img/125221-eyeglasses-angle-view.avif";
+import pic8 from "../img/125221-eyeglasses-front-view.avif";
+import pic9 from "../img/125221-eyeglasses-side-view.avif";
+
+import pic11 from "../img/2020123-eyeglasses-angle-view.avif";
+import pic12 from "../img/2020123-eyeglasses-front-view.avif";
+import pic13 from "../img/2020123-eyeglasses-side-view.avif";
+
 
 const ProductList = [
     {
         id: "GM001",
         name: "Graphite Point Guard Glasses",
         price: 15.95,     
-        rating: 4.6,      
-        image: pic1,
+        rating: 4.6,     
+        review: 790, 
+        image: [pic1,pic2,pic3],
     },
     {
         id: "GM002",
         name: "Black Rectangle Glasses",
         price: 18.5,
         rating: 4.8,
-        image: pic4,
+        review: 662,
+        image: [pic4,pic5,pic6],
     },
     {
         id: "GM003",
         name: "Black Klutch Player Glasses",
         price: 14.95,
         rating: 4.5,
-        image: pic7,
+        review: 562,
+        image: [pic7,pic8,pic9],
     },
     {
         id: "GM004",
         name: "Clear Square Glasses",
         price: 15.95,
         rating: 4.5,
-        image: pic11,
+        review: 862,
+        image: [pic11,pic12,pic13],
     },
 ];
 
