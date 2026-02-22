31190ed447495064a152b8b7804301a2b11e6767
NA <nguyenchaunhatanh1008@gmail.com>
2026-02-22 17:15:24 +0700

    add detailed product data for product detail tabs

--- Spectra_frontend/src/components/product/data/ProductList.jsx
+++ Spectra_frontend/src/components/product/data/ProductList.jsx
@@ -3,7 +3,7 @@ import pic2 from "../img/3217012-eyeglasses-front-view.avif";
 import pic3 from "../img/3217012-eyeglasses-side-view.avif";
 
 import pic4 from "../img/2010621-eyeglasses-angle-view.avif";
-import pic5 from "../img/2010621-eyeglasses-front-view.avif";
+import pic5 from "../img/unnamed (3).jpg";
 import pic6 from "../img/2010621-eyeglasses-side-view.avif";
 
 import pic7 from "../img/125221-eyeglasses-angle-view.avif";
@@ -11,43 +11,256 @@ import pic8 from "../img/125221-eyeglasses-front-view.avif";
 import pic9 from "../img/125221-eyeglasses-side-view.avif";
 
 import pic11 from "../img/2020123-eyeglasses-angle-view.avif";
-import pic12 from "../img/2020123-eyeglasses-front-view.avif";
+import pic12 from "../img/unnamed (4).jpg";
 import pic13 from "../img/2020123-eyeglasses-side-view.avif";
 
+import pic14 from "../img/unnamed (1).jpg";
+import pic15 from "../img/unnamed (2).jpg";
+import pic16 from "../img/d39cc3ad-f70c-475a-981e-0577fdce8562.png";
+import pic17 from "../img/unnamed.jpg";
 
 const ProductList = [
     {
         id: "GM001",
         name: "Graphite Point Guard Glasses",
-        price: 15.95,     
-        rating: 4.6,     
-        review: 790, 
-        image: [pic1,pic2,pic3],
+        price: 15.95,
+        rating: 4.6,
+        image: [pic1, pic2, pic3],
+        reviews: 790,
+
+        detail: {
+            fit: {
+                prescription: [
+                    "Rx Range: -13.00 ~ +7.50",
+                    "PD Range: 63 – 79 mm",
+                    "Progressive: Yes",
+                    "Bifocal: Yes",
+                ],
+                frameSize: [
+                    "Size: Large",
+                    "Frame Width: 134 mm / 5.3 in",
+                    "Bridge Width: 17 mm / 0.7 in",
+                    "Lens Width: 56 mm / 2.2 in",
+                    "Lens Height: 37 mm / 1.5 in",
+                    "Temple Length: 150 mm / 5.9 in",
+                ],
+                image: pic14,
+            },
+
+            features: {
+                frameDesign: [
+                    "Shape: Rectangle",
+                    "Rim: Full Rim",
+                    "Material: Stainless Steel",
+                    "Weight: Lightweight(15 grams / 0.5 ounces)",
+                ],
+                lensCompatibility: [
+                    "Sunglasses",
+                    "Transitions",
+                    "Blue-light blocking",
+                ],
+                whatMakesItSpecial: [
+                    {
+                        title: "Cassell x Zenni",
+                        desc: "Legendary NBA champion & visionary coach Sam Cassell’s pick for next-level style.",
+                    },
+                    {
+                        title: "Roomy Comfort",
+                        desc: "Designed with wider temples and bridges for larger faces that need extra space.",
+                    },
+                    {
+                        title: "Custom comfort",
+                        desc: "Personalize your fit for maximum stability on the move.",
+                    },
+                ],
+            },
+
+            description:
+                "Elevate your look with the Sam Cassell collection's large rectangular frame, meticulously crafted from stainless steel, featuring a full-rim design that exudes modern sophistication. This frame includes comfortable nose pads and a large size for an accommodating fit, making it perfect for everyday use by men. Designed to offer a sophisticated and elegant style, it is particularly suited for round and oval face shapes, enhancing your distinguished look. With its blend of style and comfort, this frame is an impeccable choice for those seeking a refined and contemporary appearance.",
+        },
     },
+
     {
         id: "GM002",
         name: "Black Rectangle Glasses",
         price: 18.5,
         rating: 4.8,
-        review: 662,
-        image: [pic4,pic5,pic6],
+        image: [pic4, pic5, pic6],
+        reviews: 890,
+
+        detail: {
+            fit: {
+                prescription: [
+                    "Rx Range: -13.00 ~ +5.00",
+                    "PD Range: 62 – 79 mm",
+                    "Progressive: Yes",
+                    "Bifocal: Yes",
+                ],
+                frameSize: [
+                    "Size: Large",
+                    "Frame Width: 133 mm / 5.2 in",
+                    "Bridge Width: 16 mm / 0.6 in",
+                    "Lens Width: 56 mm / 2.2 in",
+                    "Lens Height: 37 mm / 1.5 in",
+                    "Temple Length: 140 mm / 5.5 in",
+                ],
+                image: pic15,
+            },
+
+            features: {
+                frameDesign: [
+                    "Shape: Rectangle",
+                    "Rim: Full Rim",
+                    "Material: Plastic",
+                    "Weight: Lightweight(12 grams / 0.4 ounces)",
+                ],
+                lensCompatibility: [
+                    "Sunglasses",
+                    "Transitions",
+                    "Blue-light blocking",
+                ],
+                whatMakesItSpecial: [
+                    {
+                        title: "Made for all faces",
+                        desc: "Designed to accommodate many face shapes and sizes.",
+                    },
+                    {
+                        title: "Smart Value",
+                        desc: "Advanced plastic materials deliver trendy styles and reliable durability at accessible prices.",
+                    },
+                    {
+                        title: "All-day ease",
+                        desc: "Lightweight frames make all-day wear easy, no slipping or digging.",
+                    },
+                ],
+            },
+
+            description:
+                "Discover sleek sophistication with the large rectangular frame from the nan collection, meticulously crafted from durable plastic and featuring a full-rim design that exudes modern elegance. This frame includes spring hinges, a universal bridge fit, and lightweight construction, ensuring exceptional comfort and extended wear. Designed as everyday glasses for men, it offers a sophisticated and classic elegance, making it ideal for those with round face shapes. Experience the perfect blend of style and practicality with this refined frame, tailored for both comfort and timeless appeal.",
+        },
     },
+
     {
         id: "GM003",
         name: "Black Klutch Player Glasses",
         price: 14.95,
         rating: 4.5,
-        review: 562,
-        image: [pic7,pic8,pic9],
+        image: [pic7, pic8, pic9],
+        reviews: 680,
+
+        detail: {
+            fit: {
+                prescription: [
+                    "Rx Range: -20.00 ~ +12.00",
+                    "PD Range: 55 - 79 mm",
+                    "Progressive: Yes",
+                    "Bifocal: Yes",
+                ],
+                frameSize: [
+                    "Size: Large",
+                    "Frame Width: 126 mm / 5 in",
+                    "Bridge Width: 20 mm / 0.8 in",
+                    "Lens Width: 50 mm / 2 in",
+                    "Lens Height: 40 mm / 1.6 in",
+                    "Temple Length: 147 mm / 5.8 in",
+                ],
+                image: pic16,
+            },
+
+            features: {
+                frameDesign: [
+                    "Shape: Square",
+                    "Rim: Full Rim",
+                    "Material: Plastic",
+                    "Weight: (23 grams / 0.8 ounces)",
+                ],
+                lensCompatibility: [
+                    "Sunglasses",
+                    "Transitions",
+                    "Blue-light blocking",
+                ],
+                whatMakesItSpecial: [
+                    {
+                        title: "Cassell x Zenni",
+                        desc: "Legendary NBA champion & visionary coach Sam Cassell’s pick for next-level style.",
+                    },
+                    {
+                        title: "Made for all faces",
+                        desc: "Designed to accommodate many face shapes and sizes.",
+                    },
+                    {
+                        title: "Smart Value",
+                        desc: "Advanced plastic materials deliver trendy styles and reliable durability at accessible prices.",
+                    },
+                ],
+            },
+
+            description:
+                "Discover sleek sophistication with the large rectangular frame from the nan collection, meticulously crafted from durable plastic and featuring a full-rim design that exudes modern elegance. This frame includes spring hinges, a universal bridge fit, and lightweight construction, ensuring exceptional comfort and extended wear. Designed as everyday glasses for men, it offers a sophisticated and classic elegance, making it ideal for those with round face shapes. Experience the perfect blend of style and practicality with this refined frame, tailored for both comfort and timeless appeal.",
+        },
     },
+
     {
         id: "GM004",
         name: "Clear Square Glasses",
         price: 15.95,
         rating: 4.5,
-        review: 862,
-        image: [pic11,pic12,pic13],
+        image: [pic11, pic12, pic13],
+        reviews: 590,
+        
+        detail: {
+            fit: {
+                prescription: [
+                    "Rx Range: -20.00 ~ +12.00",
+                    "PD Range: 54 - 79 mm",
+                    "Progressive: Yes",
+                    "Bifocal: Yes",
+                ],
+                frameSize: [
+                    "Size: Medium",
+                    "Frame Width: 126 mm / 5 in",
+                    "Bridge Width: 18 mm / 0.7 in",
+                    "Lens Width: 51 mm / 2 in",
+                    "Lens Height: 42 mm / 1.7 in",
+                    "Temple Length: 145 mm / 5.7 in",
+                ],
+                image: pic17,
+            },
+
+            features: {
+                frameDesign: [
+                    "Shape: Square",
+                    "Rim: Full Rim",
+                    "Material: Plastic",
+                    "Weight: Lightweight(12 grams / 0.4 ounces)",
+                ],
+                lensCompatibility: [
+                    "Sunglasses",
+                    "Transitions",
+                    "Blue-light blocking",
+                ],
+                whatMakesItSpecial: [
+                    {
+                        title: "Made for all faces",
+                        desc: "Designed to accommodate many face shapes and sizes.",
+                    },
+                    {
+                        title: "Smart Value",
+                        desc: "Advanced plastic materials deliver trendy styles and reliable durability at accessible prices.",
+                    },
+                    {
+                        title: "All-day ease",
+                        desc: "Lightweight frames make all-day wear easy, no slipping or digging.",
+                    },
+                ],
+            },
+
+            description:
+                "Discover understated elegance with this medium square frame, crafted from durable plastic and featuring a full-rim design that combines classic form with modern appeal. With its universal bridge fit and lightweight construction, this frame ensures effortless comfort and extended wearability for both men and women. Exuding a sophisticated and effortless style, it is perfect for enhancing the features of oval or heart-shaped faces. Versatile enough for everyday wear, this frame seamlessly blends practicality with refined elegance.",
+        },
     },
 ];
+
+
 
 export default ProductList;
